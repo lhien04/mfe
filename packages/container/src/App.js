993caed8b42cc6e2b58034add709e87dbbf7c969
Hien Le <lhien04@gmail.com>
2021-10-15 01:18:02 -0700

    test marketing deployment

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there updated 03!</h1>
+      <h1>Hi there updated 04!</h1>
       <hr />
       <MarketingApp />
     </div>
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,7 +6,6 @@ import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 export default () => {
-  console.log("edit in App.js");
   return (
     <div>
       <StylesProvider>
